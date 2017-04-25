3814807a1f53b9e0f536dbb26eeccf15467f9630
StyvekDrtic <jiristepanek@hotmail.com>
2017-04-25 21:30:45 +0200

    fixy + sluzby + tashmoo

--- assets/js/app.js
+++ assets/js/app.js
@@ -26,6 +26,16 @@ $(".zavrit").click(function() {
   }, 1000);
 });
 
+$(".outerSluzby li").click(function() {
+  if ($(".sluzby").css("display") == "block") {
+    $(".sluzby").css("display", "none");
+  }
+
+  else {
+    $(".sluzby").css("display", "block");
+  }
+});
+
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
