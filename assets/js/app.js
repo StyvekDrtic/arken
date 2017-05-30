49bc717b963d0185e41975567c9cbf0dabb574b0
StyvekDrtic <jiristepanek@hotmail.com>
2017-05-30 17:05:54 +0200

    fb pixel

--- assets/js/app.js
+++ assets/js/app.js
@@ -43,3 +43,12 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 
 ga('create', 'UA-97878141-1', 'auto');
 ga('send', 'pageview');
+
+
+!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+document,'script','https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1555188471160377'); // Insert your pixel ID here.
+fbq('track', 'PageView');
